e054e64121dc348680ce1109b3b5f3219a6514a1
Haruyoshie <53919521+haruyoshie@users.noreply.github.com>
2024-02-21 11:53:10 -0500

    lighting and optimization

--- My project/Assets/SCRIPTS/Menu/InstantPlayer.cs
+++ My project/Assets/SCRIPTS/Menu/InstantPlayer.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class InstantPlayer : MonoBehaviour
 {
     public List<GameObject> Player;
+
+    public Transform PosInstant;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(Player[PlayerPrefs.GetInt("Player")]);
+        Instantiate(Player[PlayerPrefs.GetInt("Player")], PosInstant.position, PosInstant.rotation);
     }
 }
