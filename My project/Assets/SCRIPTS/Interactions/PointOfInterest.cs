6596cefa05115e3aef720e727bd10f1c7a7194d3
Haruyoshie <haruyoshie.ecimac@utadeo.edu.co>
2024-02-25 20:35:58 -0500

    fix project in general

--- My project/Assets/SCRIPTS/Interactions/PointOfInterest.cs
+++ My project/Assets/SCRIPTS/Interactions/PointOfInterest.cs
@@ -9,11 +9,11 @@ public class PointOfInterest : MonoBehaviour
 {
     public Camera camToRender;
     public GameObject uiToDisplay;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Player"))
         {
+            InvokeRepeating("ActiveInteraction", 0.1f, 0.1f);
             uiToDisplay.SetActive(true);
             InteractionManager.Instance.SetInteractState(InteractionState.StillMouseInteracting);
         }
@@ -23,13 +23,21 @@ public class PointOfInterest : MonoBehaviour
     {
         if (other.tag.Equals("Player"))
         {
+            CancelInvoke("ActiveInteraction");
             uiToDisplay.SetActive(false);
             InteractionManager.Instance.SetInteractState(InteractionState.Free);
         }
     }
 
+    public void ActiveInteraction()
+    {
+        InteractionManager.Instance.SetInteractState(InteractionState.StillMouseInteracting);
+    }
+
     public void Interaction()
     {
+        
+        InvokeRepeating("ActiveInteraction", 0.1f, 0.1f);
         camToRender.gameObject.SetActive(true);
         FindObjectOfType<ThirdPersonController>()._canMove = false;
         InteractionManager.Instance.SetInteractState(InteractionState.StillInteracting);
@@ -37,6 +45,8 @@ public class PointOfInterest : MonoBehaviour
 
     public void EndInteraction()
     {
+        
+        CancelInvoke("ActiveInteraction");
         FindObjectOfType<ThirdPersonController>()._canMove = true;
         camToRender.gameObject.SetActive(false);
         
